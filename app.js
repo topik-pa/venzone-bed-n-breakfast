b69e27ed9b302b4d31c5e71c7c2836ca79f274dd
PAVAN-WS\Marco Pavan <m.pavan@neticon.it>
2017-12-06 11:33:33 +0100

    Aggiunte le libreire jquery e bootstrap

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.use('/vendor/bootstrap', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist')));
+app.use('/vendor/jquery', express.static(path.join(__dirname, 'bower_components', 'jquery', 'dist')));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
